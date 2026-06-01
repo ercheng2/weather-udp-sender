05ba84c7ecda8697a0b9389e3e8026120f5a9e3e
ercheng2 <ercheng2@github.com>
2026-06-02 01:19:44 +0800

    fix: add missing System.Net.Http using directive

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Windows.Forms;
-using System.Linq;
 
 namespace WeatherUdpSender
 {
